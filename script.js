cea7c415afe7b227516200abe80ce0df5cfb4e8a
atomragnar <johanneshornbrinck@gmail.com>
2023-02-15 11:42:07 +0100

    added some changes

--- script.js
+++ script.js
@@ -1,26 +1,65 @@
-const pianoMap = new Map();
-const keyBindsMap = new Map()
-const buttons = document.querySelectorAll("button")
 
-pianoMap.set('wk1', new Audio("sounds/note1.mp3"))
-pianoMap.set('bk1', new Audio("sounds/note2.mp3"))
-pianoMap.set('wk2', new Audio("sounds/note3.mp3"))
-pianoMap.set('bk2', new Audio("sounds/note4.mp3"))
-pianoMap.set('wk3', new Audio("sounds/note5.mp3"))
-pianoMap.set('wk4', new Audio("sounds/note6.mp3"))
-pianoMap.set('bk3', new Audio("sounds/note7.mp3"))
-pianoMap.set('wk5', new Audio("sounds/note8.mp3"))
-pianoMap.set('bk4', new Audio("sounds/note9.mp3"))
-pianoMap.set('wk6', new Audio("sounds/note10.mp3"))
-pianoMap.set('bk5', new Audio("sounds/note11.mp3"))
-pianoMap.set('wk7', new Audio("sounds/note12.mp3"))
+const pianoSound1 = [
+    ["wk1", "sounds/note1.mp3"], ["bk1", "sounds/note2.mp3"], ["wk2", "sounds/note3.mp3"], 
+    ["bk2", "sounds/note4.mp3"], ["wk3", "sounds/note5.mp3"], ["wk4", "sounds/note6.mp3"],
+    ["bk3", "sounds/note7.mp3"], ["wk5", "sounds/note8.mp3"], ["bk4", "sounds/note9.mp3"],
+    ["wk6", "sounds/note10.mp3"], ["bk5", "sounds/note11.mp3"], ["wk7", "sounds/note12.mp3"]
+];
+
+const backgroundDesigns = [
+    ["bg0","original-background"], ["bg1","background-test1"]
+];
+
+const pianoDesigns = [
+    ["pdc","piano-classic"], ["pdgrad","piano-gradient"]
+];
+
+const pianoSoundMap1 = new Map(pianoSound1);
+const backgroundMap = new Map(backgroundDesigns);
+const pianoDesignMap = new Map(pianoDesigns);
+const background = document.querySelector('[name="background"]');
+const backDesign = document.querySelector(".back-design");
+const piano = document.querySelector('[name="piano"]');
+const pianoDesign = document.querySelector(".piano-design");
+
+
+addButtonListeners(pianoSoundMap1)
+
+
+function addButtonListeners(pianoSoundMap) {
+    let buttons = document.querySelectorAll(".keys button");
+    buttons.forEach(b => {
+        b.addEventListener("click", () => {
+            new Audio(pianoSoundMap.get(b.name)).play()
+            console.log(b.name)
+        })
+    });
+}
+
+
+function renderBackground() {
+    const currentBackground = background.className
+    background.classList.add(newDesign)
+    background.classList.remove(currentBackground)
+}
+
+function selectedBackground() {
+    return backgroundMap.get(backDesign.value)
+}
+
+function renderPiano() {
+    const currentPianoDesign = piano.className
+    piano.classList.add(selectedPianoDesign())
+    piano.classList.remove(currentPianoDesign)
+}
+
+function selectedPianoDesign() {
+    return pianoDesignMap.get(pianoDesign.value)
+}
 
 
 
-buttons.forEach(button => {
-    button.addEventListener("click", () => {
-        pianoMap.get(button.name).play()
-        console.log(button.name)
-    })
 
-});
+
+
+
